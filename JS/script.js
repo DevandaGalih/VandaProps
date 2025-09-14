8ef0438d38a5ab8232452c5f9add3ba032b882ea
Devanda Galih Ariyo Nasyah <devandakeren12@gmail.com>
2025-09-15 00:40:16 +0700

    Add files via upload

--- JS/script.js
+++ JS/script.js
@@ -49,3 +49,21 @@ new Swiper(".foto-wrapper", {
     },
   },
 });
+
+//fungsi sendMessage
+function sendMessage() {
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const message = document.getElementById("message").value;
+
+  const url =
+    "https://api.whatsapp.com/send?phone=6288996987264&text=Halo%20Admin%2C%0ASaya%20" +
+    name +
+    "%0AEmail%20saya%20" +
+    email +
+    "%0A%0APesan%3A%0A" +
+    message +
+    "";
+
+  window.open(url);
+}
